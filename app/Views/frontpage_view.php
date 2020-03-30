8e21cec8445fa6b573f835a1f446a8cd6034b49b
Aleksi <n9vual00@sutdents.oamk.fi>
2020-03-30 15:37:49 +0300

    title sivustolle

--- app/Views/frontpage_view.php
+++ app/Views/frontpage_view.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quarantine Games</title>
-</head>
-<body>
-    <h1>Quarantine Games</h1>
-</body>
-</html>
+<h1>Quarantine Games</h1>
