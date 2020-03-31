46a181ec391ab5674a3ffe4059f5424e0eb26c50
unknown <n9leja00@students.oamk.fi>
2020-03-31 12:48:26 +0300

    etusivu_pohja

--- app/Controllers/Frontpage.php
+++ app/Controllers/Frontpage.php
@@ -10,7 +10,7 @@ class Frontpage extends BaseController
 {
 public function index() {
     echo view('templates/header');
-    echo view('frontpage_view');
+    echo view('Frontpage/frontpage_view');
     echo view('templates/footer');
 }
 }
