73203b3105a83c4cd6a695e4d058008b150274ae
Aleksi <n9vual00@sutdents.oamk.fi>
2020-04-07 14:43:41 +0300

    ostoskori valmiiksi

--- app/Views/shoppingcart_view.php
+++ app/Views/shoppingcart_view.php
@@ -1,1 +1,42 @@
-<h3>Shopping cart</h3>
+<h3 class="text-light">Shopping cart</h3>
+
+<div class="row">
+    <div class="col-6 bg-warning ml-3 mb-3 cart">
+
+        <?= anchor('shoppingcart/empty', 'Empty cart'); ?>
+
+        <table class="table">
+
+            <tr>
+                <th>
+                    Title
+                </th>
+                <th>
+                    Price
+                </th>
+                <th>
+                    Amount
+                </th>
+            </tr>
+
+            <?php foreach ($products as $product) : ?>
+
+                <tr>
+                    <td>
+                        <?= $product['title'] ?>
+                    </td>
+
+                    <td>
+                        <?= $product['price'] ?> €
+                    </td>
+
+                    <td>
+                        <?= $product['amount'] ?>
+                    </td>
+                </tr>
+
+            <?php endforeach; ?>
+        </table>
+
+    </div>
+</div>
