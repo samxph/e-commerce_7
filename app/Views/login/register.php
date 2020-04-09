af6cdde5b3c78debb175269e35d28b0c12c41974
samxpp <n9hopy00@students.oamk.fi>
2020-04-09 15:52:06 +0300

    added css class and modified-colors

--- app/Views/login/register.php
+++ app/Views/login/register.php
@@ -1,28 +1,28 @@
-<h3><?= $title ?></h3>
+<h3 class="title"><?= $title ?></h3>
 <form action="/login/registration">
     <div class="col-12">
         <?= \Config\Services::validation()->listErrors(); ?>
     </div>
     <div class="form-group col-6">
-        <label>Username</label>
+        <label class="text-light">Username</label>
         <input class="form-control" name="user" placeholder="Enter username" maxlength="30">
-        <label>First name</label>
+        <label class="text-light">First name</label>
         <input class="form-control" name="fname" placeholder="Enter First name" maxlength="30">
-        <label>Last name</label>
+        <label class="text-light">Last name</label>
         <input class="form-control" name="lname" placeholder="Enter Last name" maxlength="30">
-        <label>Email</label>
+        <label class="text-light">Email</label>
         <input class="form-control" name="usermail" placeholder="Enter Email" maxlength="30">
-        <label>Address</label>
+        <label class="text-light">Address</label>
         <input class="form-control" name="useraddress" placeholder="Enter Address" maxlength="30">
-        <label>Postcode</label>
+        <label class="text-light">Postcode</label>
         <input class="form-control" name="userpostcode" placeholder="Enter Postcode" maxlength="30">
-        <label>City</label>
+        <label class="text-light">City</label>
         <input class="form-control" name="userpostoffice" placeholder="Enter city" maxlength="30">
-        <label>Phone number</label>
+        <label class="text-light">Phone number</label>
         <input class="form-control" name="userphone" placeholder="Enter Phone number" maxlength="30">
-        <label>Password</label>
+        <label class="text-light">Password</label>
         <input class="form-control" type="password" name="password" placeholder="Enter Password" maxlength="30">
-        <label>Password again</label>
+        <label class="text-light">Password again</label>
         <input class="form-control" type="password" name="confirmpassword" placeholder="Confirm your password" maxlength="30">
     <hr>
     </div>
