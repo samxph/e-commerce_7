11fdbb8e5fded470701218eef713faa7e2a43f9a
unknown <n9leja00@students.oamk.fi>
2020-04-14 11:07:17 +0300

    mainos1

--- app/Views/Frontpage/frontpage_view.php
+++ app/Views/Frontpage/frontpage_view.php
@@ -9,7 +9,7 @@
       </ol>
       <div class="carousel-inner" role="listbox">
         <div class="carousel-item">
-          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+          <img class="d-block img-fluid" src="images/mainos1.png" alt="First slide">
         </div>
         <div class="carousel-item">
           <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
