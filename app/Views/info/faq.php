bffa452bb59a937abeab7718932e4d7f2fcf2d15
samxpp <n9hopy00@students.oamk.fi>
2020-04-14 14:16:54 +0300

    initial faqs

--- app/Views/info/faq.php
+++ app/Views/info/faq.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?= $title ?></title>
 </head>
 <body>
 
-<h3 class="title">Quarantine games FAQ</h3>
+<div class="text-left col-xs-12 col-sm-6 col-md-8">
+<h3 class="title">Quarantine games FAQ</h3></div>
 <hr>
-<div class="text-info">
+
+<div class="row faq">
+<div class="text-info text-center col-xs-12 col-sm-6 col-md-8">
+
 <h4 class="font-weight-bold">Ordering and payment</h4>
 <br>
 <h5 class="font-weight-bold">1.    Q: How can I order</h6>
@@ -26,80 +30,87 @@ Traceability: You will have easy access to all of your previous orders any time 
 
 Reordering:  you can make a re-order anytime based on your previous orders by only couple of clicks. This will save time and effort as you don’t need to go through all the documents and emails from the past.
 </p>
+
 <h5 class="font-weight-bold">3.    Q: What information should I input when ordering?</h5>
-A: our online ordering system will ask for all the important information you should submit. If you have a VAT number, please remember to submit it. This will make sure the shipment is not delayed because of the lack of VAT number
+A: our online ordering system will ask for all the important information you should submit. If you have a VAT number, please remember to submit it. This will make sure the shipment is not delayed because of the lack of VAT number.
 
 <h5 class="font-weight-bold">4.    Q: What payment methods can I use?</h5>
-<p>A: You can use all the major credit cards.
+<p>A: You can use all the major credit cards, PayPal and bitcoin.
 
 If you are a customer with established customer relationship with HyTest Ltd. you are able to use invoice as a payment method on our online shopping checkout process.  If invoicing option is not activated for you although you are repeated customer, please contact hytest@hytest.fi and notify us.
 </p>
 <h5 class="font-weight-bold">5.    Q: Is VAT charged?</h5>
 <p>A: Online checkout prices already include VAT.
 </p>
-6.    Q: Can I use pro-forma invoice?
-A: Yes, you can use pro-forma invoice. Please contact hytest@hytest.fi if you want to use pro-forma invoice.
-
-7.    Q: What should I do if the payment is not accepted?
-A: Please try again in a little while. If the payment is still not accepted, please verify your account balance. If everything is as it should, but you still can't make the payment, please contact hytest@hytest.fi notify us about the problem. We can manage the order manually.
-
-8.    Q: How can I change delivery address?
-A: Sign in to your account and go to “my account”. On “my account” you can change all your contact information.
-
-9.    Q: What are the delivery charges?
-A: Delivery charges are dependent on the shipment requirements. If the products on your order are due to special requirements (for example dry ice) extra fee will be added to the shipment charges. You can see the shipping fees on the checkout process before the payment is made.
-
-10.    Q: What are the terms and conditions?
-A: You can see the terms and conditions here
-
-11.    Q: How can I get offer of bulk amounts?
-A: When you are logged in and you add products to shopping cart, you have the ability to send offer request to us using the shopping cart. You just need to add the products and quantities you are interested in and click “offer request”. We will send you an offer. Please note that this feature should only be used for bulk quantity price requests.
-
-12.    Q: Can I pay with invoice?
-A: If you have already established customer relationship with HyTest, you will be granted ability to pay using invoice. If you have ordered several times from HyTest but you don't see the invoice as a paying option on the checkout, please contact hytest@hytest.fi and we will check why the invoice is not activated on your account.
-
-13.    Q: Can I cancel my order?
-A: If you want to cancel your order, please do so as soon as possible. If we have already processed your order, you need to contact us and return the product. Please contact hytest@hytest.fi
-
-14.    Q: Do I have to order online?
-A: You can also send your order to hytest@hytest.fi. You can also order using FAX (+358 2 512 0909) or by calling. Online ordering is preferred in most cases because by ordering online, you will save time, you will have easier payment process and all the information about the order will be accessible for you anytime. Also if you want to make same order later, you can use “reorder” feature.
-
-15.    Q: Do you have the product in stock?
-A: All the products which are shown on our site are available. Order lead time depends on the products and quantities.
-
-16.    Q: How to contact customer service?
-A: If you have question regarding our online store (ordering, account questions, technical questions), please contact onlinestore@hytest.fi. 
+<h5 class="font-weight-bold">6.    Q: Can I use pro-forma invoice?<h5></h5>
+<p>A: Yes, you can use pro-forma invoice. Please contact hytest@hytest.fi if you want to use pro-forma invoice.
+</p>
+<h5 class="font-weight-bold">7.    Q: What should I do if the payment is not accepted?</h5>
+<p>A: Please try again in a little while. If the payment is still not accepted, please verify your account balance. If everything is as it should, but you still can't make the payment, please contact hytest@hytest.fi notify us about the problem. We can manage the order manually.
+</p>
+<h5 class="font-weight-bold">8.    Q: How can I change delivery address?</h5>
+<p>A: Sign in to your account and go to “my account”. On “my account” you can change all your contact information.
+</p>
+<h5 class="font-weight-bold">9.    Q: What are the delivery charges?</h5>
+<p>A: Delivery charges are dependent on the shipment requirements. If the products on your order are due to special requirements (for example dry ice) extra fee will be added to the shipment charges. You can see the shipping fees on the checkout process before the payment is made.
+</p>
+<h5 class="font-weight-bold">10.    Q: What are the terms and conditions?</h5>
+<p>A: You can see the terms and conditions here
+</p>
+<h5 class="font-weight-bold">11.    Q: How can I get offer of bulk amounts?</h5>
+<p>A: When you are logged in and you add products to shopping cart, you have the ability to send offer request to us using the shopping cart. You just need to add the products and quantities you are interested in and click “offer request”. We will send you an offer. Please note that this feature should only be used for bulk quantity price requests.
+</p>
+<h5 class="font-weight-bold">12.    Q: Can I pay with invoice?</h5>
+<p>A: If you have already established customer relationship with HyTest, you will be granted ability to pay using invoice. If you have ordered several times from HyTest but you don't see the invoice as a paying option on the checkout, please contact hytest@hytest.fi and we will check why the invoice is not activated on your account.
+</p>
+<h5 class="font-weight-bold">13.    Q: Can I cancel my order?</h5>
+<p>A: If you want to cancel your order, please do so as soon as possible. If we have already processed your order, you need to contact us and return the product. Please contact hytest@hytest.fi
+</p>
+<h5 class="font-weight-bold">14.    Q: Do I have to order online?</h5>
+<p>A: You can also send your order to hytest@hytest.fi. You can also order using FAX (+358 2 512 0909) or by calling. Online ordering is preferred in most cases because by ordering online, you will save time, you will have easier payment process and all the information about the order will be accessible for you anytime. Also if you want to make same order later, you can use “reorder” feature.
+</p>
+<h5 class="font-weight-bold">15.    Q: Do you have the product in stock?</h5>
+<p>A: All the products which are shown on our site are available. Order lead time depends on the products and quantities.
+</p>
+<h5 class="font-weight-bold">16.    Q: How to contact customer service?</h5>
+<p>A: If you have question regarding our online store (ordering, account questions, technical questions), please contact onlinestore@hytest.fi. 
 
 On pricing and shipping related issues you can contact customer support at hytest@hytest.fi or call +358-2-512-0900.
+</p>
+<h5 class="font-weight-bold">17.    Q: Can I return a product?</h5>
+<p>A: If you want to return a product, please contact hytest@hytest.fi
+</p>
 
-17.    Q: Can I return a product?
-A: If you want to return a product, please contact hytest@hytest.fi
-Your account
+<h4 class="font-weight-bold">Your account</h4>
 
-18.    Q: How do create an account?
-A: Go to this page https://www.hytest.fi/login and click “create a new account”, then just fill in all the needed information and click “create”.  After submitting the form, your account will be confirmed and you will be notified.  
-
-19.    Q: How can I retrieve my password?
-A: You can retrieve your password by clicking “forgot password?”. Instruction on password retrieval will be send to your email.
-
-20.    Q: How do I change my personal details or email address?
-A: You can easily change all your information on your account.  Go to login page (https://hytestshop.dev.eficode.fi/login) and log in, then click “my account” and “edit”. Here you can change all your contact information.
-
-21.    Q: Security
-A: Our web store is secured with SSL certificate. This means the information you input is encrypted and it will not be available for third parties.
+<h5 class="font-weight-bold">18.    Q: How do create an account?</h5>
+<p>A: Go to this page https://www.hytest.fi/login and click “create a new account”, then just fill in all the needed information and click “create”.  After submitting the form, your account will be confirmed and you will be notified.  
+</p>
+<h5 class="font-weight-bold">19.    Q: How can I retrieve my password?</h5>
+<p>A: You can retrieve your password by clicking “forgot password?”. Instruction on password retrieval will be send to your email.
+</p>
+<h5 class="font-weight-bold">20.    Q: How do I change my personal details or email address?</h5>
+<p>A: You can easily change all your information on your account.  Go to login page (https://hytestshop.dev.eficode.fi/login) and log in, then click “my account” and “edit”. Here you can change all your contact information.
+</p>
+<h5 class="font-weight-bold">21.    Q: Security</h5>
+<p>A: Our web store is secured with SSL certificate. This means the information you input is encrypted and it will not be available for third parties.
 Delivery
+</p>
 
-22.    Q: Can I track my order?
-A: We will send you the tracking code of the shipment when the parcel has been sent.
-
-23.    Q: Shipping time?
-A: Shipping time will be confirmed on the order confirmation document.
-
-24.    Q: Shipping cost?
-A: Shipping costs are dependent on your location and products on your order. Some products need to be shipped in dry ice. These dry ice shipments have a slightly higher shipping fee. Our online store shows the shipping fee and shipping cost automatically on the checkout.
-
-25.    Q: Changing the shipping address?
-A: You can easily change your shipping address on your account. You just need to login and click “my account” and “edit”.  You can also change the shipping address during the checkout process if you need. Just click “edit” below “shipping address”.
+<h5 class="font-weight-bold">22.    Q: Can I track my order?</h5>
+<p>A: We will send you the tracking code of the shipment when the parcel has been sent.
+</p>
+<h5 class="font-weight-bold">23.    Q: Shipping time?</h5>
+<p>A: Shipping time will be confirmed on the order confirmation document.
+</p>
+<h5 class="font-weight-bold">24.    Q: Shipping cost?</h5>
+<p>A: Shipping costs are dependent on your location and products on your order. Some products need to be shipped in dry ice. These dry ice shipments have a slightly higher shipping fee. Our online store shows the shipping fee and shipping cost automatically on the checkout.
+</p>
+<h5 class="font-weight-bold">25.    Q: Changing the shipping address?</h5>
+<p>A: You can easily change your shipping address on your account. You just need to login and click “my account” and “edit”.  You can also change the shipping address during the checkout process if you need. Just click “edit” below “shipping address”.
+</p>
 </div>
+</div>
+
 </body>
 </html>
