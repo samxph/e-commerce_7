dcb1d9205edd522392c3dad3416cd53b1d6dcadf
samxpp <n9hopy00@students.oamk.fi>
2020-04-16 15:17:33 +0300

    aboutus

---
+++ app/Views/info/about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+
+<div class="text-left col-xs-12 col-sm-6 col-md-8">
+<h3 class="title">About us</h3></div>
+<hr>
+
+<div class="row faq">
+<div class="text-info text-center col-xs-12 col-sm-6 col-md-8">
+
+<h4 class="font-weight-bold">Welcome to Quarantine games online shop!</h4>
+<br>
+<h5 class="font-weight-bold">We are online store for gaming products.</h6>
+
+<p>You can get the most games from us as digital or physical form with reasonable prices.
+</p>
+
+<h5 class="font-weight-bold">Who we are?</h5>
+<p>
+We are group of Information Technology students.
+</p>
+
+        <img src=<?php echo base_url("images\office_building.jpg") ?> alt="">
+
+</div>
+</div>
+
+</body>
+</html>
