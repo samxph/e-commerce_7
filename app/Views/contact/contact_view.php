db8d41781454071730eb6422c533d6710f251ad1
samxpp <n9hopy00@students.oamk.fi>
2020-04-20 14:23:18 +0300

    visual changes to match other pages

--- app/Views/contact/contact_view.php
+++ app/Views/contact/contact_view.php
@@ -9,16 +9,16 @@
     <div class="form-group col-4">
 
         <label class="text-light">Name:</label>
-        <input class="form-control" name="name" placeholder="Enter your full name" maxlength="100">
+        <input class="form-control" name="name" placeholder="Enter Your full name" maxlength="100">
 
         <label class="text-light">Email:</label>
         <input class="form-control" type="email" name="email" placeholder="Example@email.com" maxlength="100">
 
         <label class="text-light">Subject:</label>
-        <input class="form-control" name="subject" placeholder="Enter subject" maxlength="50">
+        <input class="form-control" name="subject" placeholder="Enter Subject" maxlength="50">
 
         <label class="text-light">Message:</label> <br>
-        <textarea type="text" name="message" placeholder="Enter Message" rows="10" cols="85" maxlength="500"></textarea>
+        <textarea type="text" name="message" placeholder="&nbsp;&nbsp;Enter Message" rows="10" cols="85" maxlength="500"></textarea>
 
         <button class="btn btn-warning mt-1">Send</button>
     </div>
