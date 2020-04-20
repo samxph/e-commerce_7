67c358d1d6191d8e9ad6678def252e90886c32e4
unknown <n9leja00@students.oamk.fi>
2020-04-20 14:26:27 +0300

    merge

--- app/Controllers/order.php
+++ app/Controllers/order.php
@@ -25,8 +25,31 @@ class Shoppingcart extends BaseController
         $this->HeaderPlatformModel = new HeaderPlatformModel();
         $this->HeaderGenreModel = new HeaderGenreModel();
     }
-    public function index()
+    public function ordersuccess()
+
     {
+        $model = new ShoppingcartAdminModel();
+
+        if (count($_SESSION['cart']) > 0) {
+            $products = $this->ShoppingcartAdminModel->getProducts($_SESSION['cart']);
+        } else {
+            $products = array();
+        }
+
+        $data2['products'] = $products;
+        $data1 = ['title' => 'Shopping cart'];
+        $data1['allPlatforms'] = $this->HeaderPlatformModel->getPlatforms();
+        $data1['allGenres'] = $this->HeaderGenreModel->getAllGenres();
+        $data1['allPlatforms'] = $this->HeaderPlatformModel->getPlatforms();
+
+        echo view('templates/header', $data1);
+        echo view('order_view', $data2);
+        echo view('templates/footer');
+
+        return redirect('checkout');
+    }
+    
+    public function index(){
         $model = new OrderModel();
         $model = new ShoppingcartAdminModel();
         $customer = [
@@ -53,28 +76,5 @@ class Shoppingcart extends BaseController
             return redirect ("/");
         
     }
-    public function ordersuccess()
-
-    {
-        $model = new ShoppingcartAdminModel();
-
-        if (count($_SESSION['cart']) > 0) {
-            $products = $this->ShoppingcartAdminModel->getProducts($_SESSION['cart']);
-        } else {
-            $products = array();
-        }
-
-        $data2['products'] = $products;
-        $data1 = ['title' => 'Shopping cart'];
-        $data1['allPlatforms'] = $this->HeaderPlatformModel->getPlatforms();
-        $data1['allGenres'] = $this->HeaderGenreModel->getAllGenres();
-        $data1['allPlatforms'] = $this->HeaderPlatformModel->getPlatforms();
-
-        echo view('templates/header', $data1);
-        echo view('order_view', $data2);
-        echo view('templates/footer');
-
-        return redirect('checkout');
-    }
-    }
+}
 
