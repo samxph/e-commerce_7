53426e096e1ab7b8b6a0334329335d555eb1d656
Aleksi <n9vual00@sutdents.oamk.fi>
2020-04-22 11:38:50 +0300

    ulkoasuun muutoksia about us -sivulla

--- app/Views/info/about.php
+++ app/Views/info/about.php
@@ -1,24 +1,24 @@
-<div class="text-left col-xs-12 col-sm-6 col-md-8">
-    <h3 class="title"><?= $title ?></h3>
-</div>
+<h3 class="title"><?= $title ?></h3>
+
 <hr>
 
-<div class="row info">
-    <div class="text-info text-center col-xs-12 col-sm-6 col-md-8">
+<div class="col-6 info cart mb-4">
+    <div class="text-info text-center">
+
+        <br>
 
         <h4 class="font-weight-bold">Welcome to Quarantine games online shop!</h4>
+
         <br>
+
         <h5 class="font-weight-bold">We are online store for gaming products.</h6>
 
-            <p>You can get the most games from us as digital or physical form with reasonable prices.
-            </p>
+            <p>You can get the most games from us as digital or physical form with reasonable prices.</p>
 
             <h5 class="font-weight-bold">Who we are?</h5>
-            <p>
-                We are group of Information Technology students.
-            </p>
 
-            <img src=<?php echo base_url("images\office_building.jpg") ?> alt="">
+            <p>We are group of Information Technology students.</p>
 
+            <img class="mb-4 cart" src=<?php echo base_url("images\office_building.jpg") ?> alt="">
     </div>
 </div>
