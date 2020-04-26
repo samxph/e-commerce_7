429f8634e0f7dbd5c8a0ff9ee911376625b0b156
samxpp <n9hopy00@students.oamk.fi>
2020-04-26 18:33:51 +0300

    added imaginary review

--- app/Database/verkkokauppa_7.sql
+++ app/Database/verkkokauppa_7.sql
@@ -1204,6 +1204,12 @@ create table review (
   saved timestamp default current_timestamp
 );
 
+
+insert into review (name, subject, message, saved)
+values ("Customer X", "This is demo subject",
+ "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+  Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.", "2020-04-26 18:14:18");
+
 /******************************************************/
 
 /* 
