6781ab2b1185f3e172f07fd4bfd9438b6f625b5b
samxpp <n9hopy00@students.oamk.fi>
2020-04-27 13:51:07 +0300

    added hr

--- app/Views/review/readreview_view.php
+++ app/Views/review/readreview_view.php
@@ -6,12 +6,14 @@
                     <div class="message info text-info">
                     <?php foreach ($reviews as $review) : ?>
 
-                        <dl>
+                <dl>
+                    
                     <dt><?= $review['name'] ?></dt>
                     <h4 class="lead"><?= $review['subject'] ?></h4>
                     <p class="font-italic"><?= $review['saved'] ?></p>
                     <p class="font-weight-light"><?= $review['message'] ?></p>
-                    </dl>
+                    <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+                </dl>
 
                     <?php endforeach; ?>
                     </div>
