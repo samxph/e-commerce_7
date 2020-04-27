1e134fb69145b2102d2b6b9d5e2619629e7a2d26
samxpp <n9hopy00@students.oamk.fi>
2020-04-27 09:59:44 +0300

    added review visuals

--- app/Views/review/readreview_view.php
+++ app/Views/review/readreview_view.php
@@ -1,15 +1,18 @@
-<div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-light">
+<div class="container">   
                     <h3 class="title"><?= $title ?></h3>
+                    &nbsp;&nbsp;<button type="button" class="btn btn-secondary" onclick="window.location='<?php echo site_url('sendreview/'); ?>'">Write new review</button>
+                    <hr>
+
+                    <div class="message info text-info">
                     <?php foreach ($reviews as $review) : ?>
 
-                    <h5> <p><?= $review['name'] ?></p></h5>
-                    <?= $review['subject'] ?>
+                    <h4> <p><?= $review['name'] ?></p></h4>
+                    <h4><?= $review['subject'] ?></h4>
                     <p><?= $review['saved'] ?></p>
                     <p><?= $review['message'] ?></p>
 
                     <?php endforeach; ?>
+                    </div>
                 </div>
-            </div>
+            
         </div>
